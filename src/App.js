54927df883df00ce829bf2d733c564f3241aa081
Sakif Hridoy <sakifhridoy@gmail.com>
2021-09-19 23:16:26 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { useState } from 'react';
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const[user,setUser] = useState({});
+  var provider = new firebase.auth.GoogleAuthProvider();
+  var fbProvider = new firebase.auth.FacebookAuthProvider();
+  var githubProvider = new firebase.auth.GithubAuthProvider();
+  const handleGoogleSignIn = ()=>{
+     firebase.auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+  
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      setUser(user);
+      console.log(user,token)
+    }).catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      console.log(credential,errorCode,errorMessage,email)
+      
+    });
+  }
+
+  const handleFacebookSignIn = ()=>{
+  firebase
+  .auth()
+  .signInWithPopup(fbProvider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    // The signed-in user info.
+    var user = result.user;
+
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var accessToken = credential.accessToken;
+
+    // ...
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+
+    // ...
+  });
+  }
+
+
+  const handleGithubSignIn = ()=>{
+    firebase
+  .auth()
+  .signInWithPopup(githubProvider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+    var token = credential.accessToken;
+
+    // The signed-in user info.
+    var user = result.user;
+    console.log(credential,user)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2>Google Sign In</h2>
+     <button onClick={handleGoogleSignIn}>Sign in with Google</button><br/>
+     <button onClick={handleFacebookSignIn}>Sign in with Facebook</button><br/>
+     <button onClick={handleGithubSignIn}>Sign in with Github</button>
+     <h2>{user.email}</h2>
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDQWYvNgGGWYgzAOleO0WmmRIhEyrlTTKc",
+  authDomain: "auth-e8d31.firebaseapp.com",
+  projectId: "auth-e8d31",
+  storageBucket: "auth-e8d31.appspot.com",
+  messagingSenderId: "918312757576",
+  appId: "1:918312757576:web:b8e4a0150da413c37f2b7d"
+};
+export default firebaseConfig;
